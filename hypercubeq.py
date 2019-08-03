0b480eacb7965182cab5e5a308c2030515d0dd6b
meowoodie <woodielove@163.com>
2019-08-03 00:26:13 -0400

    almost finish the steady-state probability, some minor issue when mu is not 1 still exists.

--- hypercubeq.py
+++ hypercubeq.py
@@ -1,3 +1,4 @@
+import math
 import arrow
 import numpy as np
 from collections import defaultdict
@@ -10,7 +11,7 @@ class HypercubeQ(object):
     """
 
 
-    def __init__(self, n_atoms, Lam=None, Mu=None, T=None, P=None):
+    def __init__(self, n_atoms, Lam=None, Mu=None, T=None, P=None, max_iter=10):
         # Model configuration
         # - number of geographical atoms (response units)
         self.n_atoms = n_atoms                    
@@ -33,7 +34,7 @@ class HypercubeQ(object):
         # - upward transition rates matrix: a dictionary { (i,j) : lam_ij } (due to the sparsity of the matrix in nature)
         self.Lam_ij = self._upward_transition_rates()
         # - steady-state probability for unsaturate states
-        self.Pi     = np.zeros(2 ** self.n_atoms)
+        self.Pi     = self._steady_state_probabilities(max_iter=max_iter)
 
     def _tour(self):
         """
@@ -73,33 +74,66 @@ class HypercubeQ(object):
                     Lam_ij[(i,Upoptn[k,i])] += self.Lam[k]
         return Lam_ij
 
-    def _steady_state_probabilities(self):
+    def _steady_state_probabilities(self, cap="zero", max_iter=100):
         """
         A iterative procedure for obtaining the steady state probability on the hypercube. In a manner
         similar to point Jacobi iteration, we use the equation of detailed balance to determine the 
         values at successive iterations. 
-s
-        For S_0, S_{2^N-1} and S_Q (more than N customers in the system), the steady state probabilities can 
-        be calculated as a normal M/M/N queue with infinite-line capacity.
+
+        For all idle state, all busy state, and S_Q (more than N customers in the system), the steady   
+        state probabilities can be calculated as a normal M/M/N queue with infinite-line capacity.
         """
-        # 
-        def init_steady_state_prob():
-            return Pi
-        # point Jacobi iteration for states from S_1 to S_{2^N-2}
+        # steady state probabilities initialization
+        def init_steady_state_prob(cap):
+            Pi_0 = np.zeros(2 ** self.n_atoms)
+            for n_busy in range(self.n_atoms + 1):
+                denominator = sum([
+                    self.Lam.sum() ** j / math.factorial(j)
+                    for j in range(self.n_atoms + 1) ]) \
+                    if cap == "zero" else \
+                    sum([ (self.Lam.sum() ** j / math.factorial(j)) + 
+                    (self.Lam.sum() ** self.n_atoms / math.factorial(self.n_atoms)) * 
+                    (self.Lam.sum() / self.n_atoms / (1 - self.Lam.sum() / self.n_atoms)) 
+                    for j in range(self.n_atoms + 1) ])
+                n_states    = sum([ 1 for i in range(2 ** self.n_atoms) if self.S[i].sum() == n_busy ])
+                init_P      = (self.Lam.sum() ** n_busy / math.factorial(n_busy)) / denominator / n_states
+                for i in range(2 ** self.n_atoms):
+                    if self.S[i].sum() == n_busy:
+                        Pi_0[i] = init_P
+            return Pi_0
+
+        # point Jacobi iteration for all states except for all idle state, all busy state
         def iter_steady_state_prob(Pi_n):
             # initialize the steady state probabilities Pi_{n+1} for the next iteration
-            Pi_n_1 = np.zeros(2 ** self.n_atoms)
-            for i in range(1, 2 ** self.n_atoms-1):
-                Pi_n_1[i] = #....
+            Pi_n_1 = np.copy(Pi_n)
+            # update the steady state probability of each state i
+            for j in range(2 ** self.n_atoms):
+                # except for all idle state, all busy state
+                if self.S[j].sum() != 0 and self.S[j].sum() != self.n_atoms:
+                    upward_sum   = 0
+                    downward_sum = 0
+                    for i in range(2 ** self.n_atoms):
+                        # the k-th response unit that changed status (upward and downward in total)
+                        changed_k = np.nonzero(self.S[i] - self.S[j])[0]
+                        # only consider one-step changed states
+                        if len(changed_k) == 1:
+                            # upward transition state
+                            if (self.S[j] - self.S[i]).sum() == 1:
+                                upward_sum += Pi_n[i] * self.Lam_ij[(i,j)]
+                            # downward transition state
+                            elif (self.S[j] - self.S[i]).sum() == -1:
+                                downward_sum += Pi_n[i] * self.Mu[changed_k[0]]
+                    # update the j-th state in Pi_{n+1}
+                    Pi_n_1[j] = (upward_sum + downward_sum) / (self.Lam.sum() + (self.Mu * self.S[j]).sum())
             return Pi_n_1
-
-        max_iter = 100
-        Pi       = init_steady_state_prob()
-        Pi[0]    = ...
-        Pi[-1]   = ...
+        
+        # initialize all states
+        Pi = init_steady_state_prob(cap)
+        print(Pi)
+        print(Pi.sum())
+        # update all states except for all idle state, all busy state
         for n in range(max_iter):
             Pi = iter_steady_state_prob(Pi)
-        
         return Pi
 
     def __upward_optimal_neighbor(self):
@@ -128,7 +162,7 @@ s
                             Upopts[k,i] = state_index(upopts)
                             break
                 else:
-                    Upopts[k,i] = -1                 # for the last state, there is no upward neighbor
+                    Upopts[k,i] = -1                 # for the all busy state, there is no upward neighbor
         return Upopts
 
 
@@ -149,19 +183,19 @@ if __name__ == "__main__":
                [2, 0, 1]]
 
 
-    # * CHECK UPWARD TRANSITION RATES
-    hq = HypercubeQ(n_atoms, Lam=Lam, Mu=Mu, P=P)
-    print(hq.S)
-    state_order = [0, 7, 3, 1, 4, 6, 2, 5]
-    Lam_ij_dict = hq._upward_transition_rates()
-    Lam_ij_mat  = np.zeros((2 ** hq.n_atoms, 2 ** hq.n_atoms))
-    for key in Lam_ij_dict:
-        i = state_order.index(key[0])
-        j = state_order.index(key[1])
-        Lam_ij_mat[i,j] = Lam_ij_dict[key]
-    print(Lam_ij_mat)
+    # # * CHECK UPWARD TRANSITION RATES
+    # hq = HypercubeQ(n_atoms, Lam=Lam, Mu=Mu, P=P)
+    # print(hq.S)
+    # state_order = [0, 7, 3, 1, 4, 6, 2, 5]
+    # Lam_ij_dict = hq._upward_transition_rates()
+    # Lam_ij_mat  = np.zeros((2 ** hq.n_atoms, 2 ** hq.n_atoms))
+    # for key in Lam_ij_dict:
+    #     i = state_order.index(key[0])
+    #     j = state_order.index(key[1])
+    #     Lam_ij_mat[i,j] = Lam_ij_dict[key]
+    # print(Lam_ij_mat)
 
-    # * CHECK UPWARD OPTIMAL NEIGHBORS
+    # # * CHECK UPWARD OPTIMAL NEIGHBORS
     # hq  = HypercubeQ(n_atoms, Lam=Lam, Mu=Mu, P=P)
     # print(hq.S)
     # Upn = hq._upward_optimal_neighbor()
@@ -171,3 +205,8 @@ if __name__ == "__main__":
     # print(k)
     # print("neighbors")
     # print(hq.S[Upn[k, i]])
+
+    # * CHECK STEADY STATE PROBABILITY
+    hq = HypercubeQ(n_atoms=5, Mu=np.ones(5), max_iter=10)
+    print(hq.Pi)
+    print(hq.Pi.sum())
